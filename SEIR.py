78313293b7deefc6421653e64c30c565a267ad0a
雪山飞狐 <49848629+bevarb@users.noreply.github.com>
2020-06-05 21:44:01 +0800

    UPdate SEIR

--- SEIR.py
+++ SEIR.py
@@ -18,18 +18,25 @@ class Learner(object):
         self.s_0 = N - e_0 - i_0 - r_0
         self.ratio = ratio
 
-    def __load_data__(self, root):
+    def __load_data__(self, root, begain, over):
         df = pd.read_csv(root)
         df.set_index(["Date"], inplace=True)
-        confirmed = df["Confirmed"].T
-        exposed = confirmed * self.ratio
-        recovered = df["Recovered"].T
-        dead = df["Dead"].T
-        susceptible = self.N - confirmed - exposed - recovered - dead
+        self.Real_data = df
+        confirmed = df.loc[begain:over, "Confirmed"].T  # 所有确诊的，后面尽管变为R or D ，数目是累计的
+        exposed = df.loc[begain:over, "Exposed"].T  # 将确诊的往前推5天作为Exposed
+        recovered = df.loc[begain:over, "Recovered"].T
+        dead = df.loc[begain:over, "Dead"].T
+        susceptible = np.ones(len(confirmed)) * self.N - confirmed - exposed - recovered - dead
         return susceptible, exposed, confirmed, recovered, dead
 
 
     def extend_index(self, index, new_size):
+        '''
+        extend index for plot
+        :param index: smaller index
+        :param new_size: bigger index
+        :return:
+        '''
         values = index.values
         current = datetime.strptime(index[-1], '%m/%d/%y')
         while len(values) < new_size:
@@ -37,11 +44,16 @@ class Learner(object):
             values = np.append(values, datetime.strftime(current, '%m/%d/%y'))
         return values
 
-    def predict(self, beta, alpha, gamma, data, exposed, recovered, death, s_0, e_0, i_0, r_0):
-        new_index = self.extend_index(data.index, self.predict_range)
+    def extend_data(self, data):
+        new_index = self.extend_index(data.index, len(data.index) + self.predict_range)
         size = len(new_index)
-        N = self.N
+        extended = np.concatenate((data.values, [None] * (size - len(data.values))))
+        return extended
 
+    def predict(self, beta, alpha, gamma, size, s_0, e_0, i_0, r_0):
+        '''input parameters to predict'''
+
+        N = self.N
         def SEIR(t, y):
             S = y[0]
             E = y[1]
@@ -49,78 +61,80 @@ class Learner(object):
             R = y[3]
             return [-beta * S * I / N, beta * S * I / N - alpha * E, alpha * E - gamma * I, gamma * I]
 
-        extended_actual = np.concatenate((data.values, [None] * (size - len(data.values))))
-        extended_exposed = np.concatenate((exposed.values, [None] * (size - len(exposed.values))))
-        extended_recovered = np.concatenate((recovered.values, [None] * (size - len(recovered.values))))
-        extended_death = np.concatenate((death.values, [None] * (size - len(death.values))))
-        return new_index, extended_actual, extended_exposed, extended_recovered, extended_death, solve_ivp(SEIR,[0, size],
-                                                                                         [s_0, e_0,i_0, r_0],
-                                                                                         t_eval=np.arange(0, size, 1))
+        return solve_ivp(SEIR, [0, size], [s_0, e_0, i_0, r_0], t_eval=np.arange(0, size, 1))
 
-    def train(self):
-        susceptible, exposed, confirmed, recovered, dead = self.__load_data__('./Data/US_All.csv')
+    def train(self, root, start, over):
+        susceptible, exposed, confirmed, recovered, dead = self.__load_data__(root, start, over)
 
-        optimal = minimize(self.loss, np.array([0.001, 0.001, 0.001]),
-                           args=(susceptible, exposed, recovered, self.s_0, self.e_0, self.i_0, self.r_0),
-                           method='L-BFGS-B', bounds=[(0.00000001, 0.6), (0.00000001, 0.4), (0.00000001, 0.4)])
+        optimal = minimize(self.loss, np.array([0.2, 0.2, 0.2]),
+                           args=(susceptible, exposed, confirmed - recovered - dead, recovered + dead,
+                                 self.s_0, self.e_0, self.i_0, self.r_0),
+                           method='L-BFGS-B', bounds=[(0.00000001, 0.8), (0.14, 0.33), (0.35, 0.6)])
 
         print(optimal)
 
-        '''预测数据'''
+        new_index = self.extend_index(susceptible.index, len(susceptible.index) + self.predict_range)
+        '''Predict'''
         beta, alpha, gamma = optimal.x
-        new_index, extended_actual, extended_exposed, extended_recovered, extended_death, prediction = \
-            self.predict(beta, alpha, gamma, susceptible, exposed, recovered, dead,
-                                             self.s_0, self.e_0, self.i_0, self.r_0)
+        print(f" beta={beta:.8f}, alpha={alpha:.8f}, gamma={gamma:.8f}, r_0:{(beta / gamma):.8f}")
+        '''Plot'''
+        prediction = self.predict(beta, alpha, gamma, len(new_index), self.s_0, self.e_0, self.i_0, self.r_0)
+        # extend all data
+        extended_confirmed = self.extend_data(confirmed)
+        extended_recovered = self.extend_data(recovered)
+        extended_death = self.extend_data(dead)
+        # extended_confirmed = self.extend_data(self.Real_data["Confirmed"].T)
+        # extended_recovered = self.extend_data(self.Real_data["Recovered"].T)
+        # extended_death = self.extend_data(self.Real_data["Dead"].T)
         df = pd.DataFrame(
             {
-            # 'Infected data': extended_actual,
-             'Recovered data': extended_recovered,
-             'Death data': extended_death,
+             'Real Infected': extended_confirmed,
+             'Real Recovered': extended_recovered,
+             'Real Death': extended_death,
              #'Susceptible': prediction.y[0],
-             'Exposed': prediction.y[1],
-             'Infected': prediction.y[2],
-             'Recovered': prediction.y[3]},
+             #'Predict Exposed': prediction.y[1],
+             'Predict Infected': prediction.y[2],
+             'Rredict Recovered': prediction.y[3]},
             index=new_index)
         fig, ax = plt.subplots(figsize=(15, 10))
         #ax.set_title(self.country)
         df.plot(ax=ax)
-        print(f" beta={beta:.8f}, alpha={alpha:.8f}, gamma={gamma:.8f}, r_0:{(beta / gamma):.8f}")
         fig.savefig("result_SEIR.png")
         fig.show()
-        print(df)
         df.to_csv('result_SEIR.csv')
 
-    def loss(self, point, susceptible, exposed, recovered, s_0, e_0, i_0, r_0):
+    def loss(self, point, susceptible, exposed, infected, removed, s_0, e_0, i_0, r_0):
         size = len(susceptible)
         beta, alpha, gamma = point
         N = self.N
-
 
         def SEIR(t, y):
             S = y[0]
             E = y[1]
             I = y[2]
             R = y[3]
-            return [-beta * S * I / N, beta * S * I / N - alpha * E, alpha * E - gamma * I, gamma * I]
+            return [-beta * S * (I + E) / N, beta * S * I / N - alpha * E, alpha * E - gamma * I, gamma * I]
 
         solution = solve_ivp(SEIR, [0, size], [s_0, e_0, i_0, r_0], t_eval=np.arange(0, size, 1), vectorized=True)
-        l1 = np.sqrt(np.mean((solution.y[1] - susceptible) ** 2))
-        l2 = np.sqrt(np.mean((solution.y[2] - exposed) ** 2))
-        l3 = np.sqrt(np.mean((solution.y[3] - recovered) ** 2))
-        a1 = 0.1
-        a2 = 0.1
-        return a1 * l1 + a2 * l2 + (1 - a1 - a2) * l3
+        # l1 = np.sqrt(np.mean((solution.y[0] - susceptible) ** 2))
+        l2 = np.sqrt(np.mean((solution.y[1] - exposed) ** 2))
+        l3 = np.sqrt(np.mean((solution.y[2] - infected) ** 2))
+        l4 = np.sqrt(np.mean((solution.y[3] - removed) ** 2))
+        a1 = 0.4
+        a2 = 0.4
+        a3 = 0.1
+        return a2 * l2 + a3 * l3 + (1 - a1 - a2 - a3) * l4
 
 
 
 
 predict_range = 30
-N = 3.282 * 10 ** 9
-s_0 = 99990  # 易感人数
-e_0 = 8  # 潜伏者
-i_0 = 2  # 感染者
+N = 3.282 * (10 ** 9)
+
+e_0 = 2  # 潜伏者
+i_0 = 4  # 感染者
 r_0 = 0  # 康复者
-ratio = 0.3
+ratio = 0.25
 
 learner = Learner(predict_range, e_0, i_0, r_0, N, ratio)
-learner.train()
+learner.train('./Data/Us_All.csv', "3/22/20", "5/29/20")
